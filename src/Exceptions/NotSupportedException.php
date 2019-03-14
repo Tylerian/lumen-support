03365332bde87b82498512765319f2195fe8449f
Tylerian <jairo.eog@outlook.com>
2019-03-14 23:43:07 +0100

    Added NotSupportedException.

---
+++ src/Exceptions/NotSupportedException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tylerian\Lumen\Support\Exceptions;
+
+/**
+ * Exception class generated when trying to use features not
+ * supported by certain classes or abstractions.
+ */
+class NotSupportedException extends \BadMethodCallException
+{
+}
